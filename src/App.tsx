dd39f5a5b76989c5151f2d0a439e60c951ca9e6c
Vladislav Flinski <flinssski@gmail.com>
2025-11-24 22:22:42 +0300

    feat: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+
+import AppLayout from '@/ui/AppLayout'
+import DashboardPage from '@/pages/DashboardPage'
+
 export default function App() {
-	return <div className="font-inter overflow-hidden antialiased">Hello World!</div>
+	return (
+		<div className="font-inter text-ui-950 bg-ui-50 antialiased">
+			<BrowserRouter>
+				<Routes>
+					<Route element={<AppLayout />}>
+						<Route index element={<Navigate replace to="dashboard" />} />
+						<Route path="dashboard" element={<DashboardPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
